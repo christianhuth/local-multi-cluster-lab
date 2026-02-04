d9f36c2301e53c25a9309e7306e70a45ee320daf
christian.huth <christian.huth@proact.eu>
2026-02-04 17:01:37 +0100

    comment kubeconfig commands

--- start-lab.sh
+++ start-lab.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 kind create cluster --config cluster-a.yaml
-kind get kubeconfig --name cluster-a > ~/.kube/configs/kind/local-multi-cluster-a.yaml
-
 kind create cluster --config cluster-b.yaml
-kind get kubeconfig --name cluster-b > ~/.kube/configs/kind/local-multi-cluster-b.yaml
-
 kind create cluster --config cluster-c.yaml
-kind get kubeconfig --name cluster-c > ~/.kube/configs/kind/local-multi-cluster-c.yaml
+
+# kind get kubeconfig --name cluster-a > ~/.kube/configs/kind/local-multi-cluster-a.yaml
+# kind get kubeconfig --name cluster-b > ~/.kube/configs/kind/local-multi-cluster-b.yaml
+# kind get kubeconfig --name cluster-c > ~/.kube/configs/kind/local-multi-cluster-c.yaml
